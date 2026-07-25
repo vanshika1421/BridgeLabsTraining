68529eb9c3d6b067eeca2068d566fab443c4a3b1
vanshika <vanshika1407@gmail.com>
2026-07-25 14:35:54 +0530

    Control flow level 2 added

---
+++ Control_Flow_Level2/Control_Flow_Level2/FactorsOfnumber.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Control_Flow_Level2
+{
+    internal class FactorsOfnumber
+    {
+    }
+}
---
+++ Control_Flow_Level2/Control_Flow_Level2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Control_Flow_Level2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          MultiplesOfNumber.FindMultiples();
+        }
+    }
+}
